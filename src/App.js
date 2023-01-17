07947e9c84c0fb02d803c5e8f20867d9162bf1a5
Uliana Cheklina <uche0790@gmail.com>
2023-01-17 22:44:12 +0100

    Manage the last changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
-import { }// emotion from lecture 17.02
-
 // pressing the generate button should generate a new color: choose the library to generate random color
 // div should take the generated color as background color and print out the hex name of the color.
-function App(props) {
+export default function App() {
   const [color, setColor] = useState('');
   return (
-    <div>
-      <button style={{ border: '5px solid purple', borderRadius: '10%' }}
-      onClick={() => setColor(randomColor())}>Generate</button
-      <p>Generated Color:background color {props.color}</p>
-   </div>
+    <div
+      style={{
+        backgroundColor: `${color}`,
+        border: '2px solid #14213d',
+        letterSpacing: '2px',
+        borderRadius: '15px',
+        margin: '20px',
+        padding: '20px',
+        fontWeight: '500',
+      }}
+    >
+      <input
+        style={{
+          borderRadius: '7px',
+        }}
+        value={color}
+        onChange={(event) => {
+          console.log('event.currentTarget.value', event.currentTarget.value);
+        }}
+      />
+      <br />
+      <button
+        style={{
+          backgroundColor: '#14213d',
+          color: 'white',
+          borderRadius: '7px',
+          letterSpacing: '2px',
+        }}
+        onClick={() => setColor(randomColor())}
+      >
+        Generate
+      </button>
+
+      <p>Generated Color: background-color {color}</p>
+    </div>
   );
 }
