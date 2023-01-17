43ce3cbd6590c27c8b9ce74a9b7122df5e6976fd
Uliana Cheklina <uche0790@gmail.com>
2023-01-17 15:17:57 +0100

    Make from 2 js files one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+// import './App.css';
 import randomColor from 'randomcolor';
-import Button from './Button.js';
+import { useState } from 'react';
 
-export default function App(props) {
+import { }// emotion from lecture 17.02
+
+// pressing the generate button should generate a new color: choose the library to generate random color
+// div should take the generated color as background color and print out the hex name of the color.
+function App(props) {
+  const [color, setColor] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button />
-        <p>Generated Color:background color {props.color}</p>
-      </header>
-    </div>
+    <div>
+      <button style={{ border: '5px solid purple', borderRadius: '10%' }}
+      onClick={() => setColor(randomColor())}>Generate</button
+      <p>Generated Color:background color {props.color}</p>
+   </div>
   );
 }
